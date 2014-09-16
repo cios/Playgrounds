4ebb15ef54078cb2e9b66b0b20184a284275f093
Daniel Esquivias <danielesquivias@daniels-mbp.hsd1.ca.comcast.net>
2014-09-16 16:55:38 -0700

    random number generator

---
+++ RandomNumberGenerator.playground/section-1.swift
@@ -0,0 +1,8 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Generates a random number from 0 to 1
+// Int() is needed to convert Unsigned Integer to Int
+let randomNumber = Int(arc4random_uniform(UInt32(2)))
+
