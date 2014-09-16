4509b19646562e98284d668545a5628eec4628c6
Daniel Esquivias <danielesquivias@daniels-mbp.hsd1.ca.comcast.net>
2014-09-16 13:24:34 -0700

    Numeric Type Conversion

---
+++ NumericTypeConversion.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var integerValue = 5
+var doubleValue = 0.5
+
+var sumOfValues = Double(integerValue) + doubleValue
+
+var secondIntegerValue = 8
+var secondDoubleValue = 1.5
+
+var sumOfSecondValues = Int(secondDoubleValue) + secondIntegerValue
