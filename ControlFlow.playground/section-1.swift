3e3515dfcb735c239c14bbba0a5c03848d91bea4
Daniel Esquivias <danielesquivias@daniels-mbp.hsd1.ca.comcast.net>
2014-09-16 13:05:16 -0700

    adding ControlFlow Playground

---
+++ ControlFlow.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let truckSpeed = 45
+let lamboSpeed = 120
+
+var mySpeed:Int
+
+mySpeed = 55
+
+if mySpeed < 70{
+    println("Keep crusing")
+}else{
+    println("Slow down police ahead")
+}
